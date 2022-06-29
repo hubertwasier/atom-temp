ec72968d32038dd051cdc72aca40c1773b5e1f34
hubertwasier <hubert@atom-dev.io>
2022-06-29 15:14:43 +0400

    Netlify-form added

--- pages/contact.js
+++ pages/contact.js
@@ -3,6 +3,7 @@ import projectsData from '@/data/projectsData'
 import Card from '@/components/Card'
 import { PageSEO } from '@/components/SEO'
 import Link from 'next/link'
+import ContactForm from '@/components/ContactForm'
 
 export default function Offres() {
   return (
@@ -14,16 +15,9 @@ export default function Offres() {
             Nos offres
           </h1>
         </div>
-        <div className="flex flex-col items-start items-center space-y-2 py-8 text-center">
-          <p className="pt-4 text-2xl font-semibold">Design. Build. Improve.</p>
-          <h2 className="leading-16 max-w-3xl pt-4 text-6xl font-bold">
-            Je crée les magnifiques sites Web que vos clients adoreront
-          </h2>
-          <Link href="/contact">
-            <a className="btn btn-primary mt-8 flex rounded-lg bg-primary-500 px-3 py-2 font-semibold text-white transition-all hover:scale-125 dark:hover:text-gray-200">
-              Allons y!
-            </a>
-          </Link>
+        <div>
+          Contact me!
+          <ContactForm />
         </div>
       </div>
     </>
